e69dfb83350df8b6dfca4d91a2c9247bec2459a9
JavaFXpert <james@j1w.xyz>
2018-12-21 06:38:40 -0500

    Add installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ibmq-anki-vector
-Experiments with Anki Vector leveraging IBM Q quantum computers
+An experiment with Anki Vector robot leveraging IBM Q quantum computers
+
+## Installation
+
+To install and run this experiment:
+
+1. Ensure that [Python 3.5 or later](https://www.python.org/downloads/) is installed
+2. Clone the [JavaFXpert/ibmq-anki-vector](https://github.com/JavaFXpert/ibmq-anki-vector) project from GitHub.
+3. Install the latest version of [Qiskit](https://qiskit.org/).
+4. Obtain an Anki Vector and install its SDK https://www.anki.com/en-us/vector
+
+## Running
+
+- From the operating system prompt in the project's root directory, run:
+
+`python vector-and-ibmq.py`
+
--- vector-and-ibmq.py
+++ vector-and-ibmq.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
+#
+# Copyright 2019 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-"""Go to pose
-
-Make Vector go to a pose.
-"""
+# vector-and-ibmq.py
+# Demonstration of an Anki Vector robot leveraging an IBM quantum computer
+# Note: For ideas on expanding the number of responses from 2 to 8 or more, see the original
+#       quantum 8-ball Jupyter notebook that Oleksii Lialka created as a Qiskit tutorial.
+# https://github.com/Qiskit/qiskit-tutorials/blob/master/community/hello_world/quantum_8ball.ipynb
 
 import anki_vector
 import os
